6fc824a30a32a9efe9c4549dc643870a0fb31911
F5OEO <evaristec@gmail.com>
2023-06-19 15:02:10 +0200

    Add revplus dts

--- pluto-buildroot/run_only_once.sh
+++ pluto-buildroot/run_only_once.sh
@@ -3,7 +3,10 @@ cp plutosdr_fw_patch/zynq_pluto_defconfig ../plutosdr-fw/linux/arch/arm/configs
 
 cp plutosdr_fw_patch/zynq-pluto-sdr-revc.dts ../plutosdr-fw/linux/arch/arm/boot/dts/
 cp plutosdr_fw_patch/zynq-pluto-sdr-revb.dts ../plutosdr-fw/linux/arch/arm/boot/dts/
+cp plutosdr_fw_patch/zynq-pluto-sdr-revplus.dts ../plutosdr-fw/linux/arch/arm/boot/dts/
 cp plutosdr_fw_patch/zynq-pluto-sdr.dtsi ../plutosdr-fw/linux/arch/arm/boot/dts/
+cp plutosdr_fw_patch/pluto.its ../plutosdr-fw/scripts
+cp plutosdr_fw_patch/pluto.mk ../plutosdr-fw/scripts
 
 cp plutosdr_fw_patch/linux/ad9361_regs.h ../plutosdr-fw/linux/drivers/iio/adc/
 cp plutosdr_fw_patch/linux/ad9361.c ../plutosdr-fw/linux/drivers/iio/adc/
