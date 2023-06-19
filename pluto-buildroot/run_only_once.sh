f03b095bd9d83674091f2b62395b0722f058fba5
F5OEO <evaristec@gmail.com>
2023-06-19 13:43:12 +0200

    Prepare for pluto+

--- pluto-buildroot/run_only_once.sh
+++ pluto-buildroot/run_only_once.sh
@@ -3,6 +3,7 @@ cp plutosdr_fw_patch/zynq_pluto_defconfig ../plutosdr-fw/linux/arch/arm/configs
 
 cp plutosdr_fw_patch/zynq-pluto-sdr-revc.dts ../plutosdr-fw/linux/arch/arm/boot/dts/
 cp plutosdr_fw_patch/zynq-pluto-sdr-revb.dts ../plutosdr-fw/linux/arch/arm/boot/dts/
+cp plutosdr_fw_patch/zynq-pluto-sdr.dtsi ../plutosdr-fw/linux/arch/arm/boot/dts/
 
 cp plutosdr_fw_patch/linux/ad9361_regs.h ../plutosdr-fw/linux/drivers/iio/adc/
 cp plutosdr_fw_patch/linux/ad9361.c ../plutosdr-fw/linux/drivers/iio/adc/
