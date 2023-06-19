a60f3cb75ef5af9a74fda4d96e687d74bb3f5730
F5OEO <evaristec@gmail.com>
2023-06-19 11:03:45 +0200

    add custom ad9361 source

--- pluto-buildroot/run_only_once.sh
+++ pluto-buildroot/run_only_once.sh
@@ -5,6 +5,7 @@ cp plutosdr_fw_patch/zynq-pluto-sdr-revc.dts ../plutosdr-fw/linux/arch/arm/boot/
 cp plutosdr_fw_patch/zynq-pluto-sdr-revb.dts ../plutosdr-fw/linux/arch/arm/boot/dts/
 
 cp plutosdr_fw_patch/linux/ad9361_regs.h ../plutosdr-fw/linux/drivers/iio/adc/
+cp plutosdr_fw_patch/linux/ad9361.c ../plutosdr-fw/linux/drivers/iio/adc/
 cp plutosdr_fw_patch/linux/f_uac2.c ../plutosdr-fw/linux/drivers/usb/gadget/function/
 
 # For dtc -@ oscimp
