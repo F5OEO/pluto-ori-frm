d2d4f4af6546fb932da58ec4d19f021c00dfe387
F5OEO <evaristec@gmail.com>
2023-06-23 12:04:48 +0200

    Add zynq common

--- pluto-buildroot/run_only_once.sh
+++ pluto-buildroot/run_only_once.sh
@@ -1,4 +1,4 @@
-cp plutosdr_fw_patch/Makefile ../plutosdr-fw/
+#cp plutosdr_fw_patch/Makefile ../plutosdr-fw/
 cp plutosdr_fw_patch/zynq_pluto_defconfig ../plutosdr-fw/linux/arch/arm/configs
 
 cp plutosdr_fw_patch/zynq-pluto-sdr-revc.dts ../plutosdr-fw/linux/arch/arm/boot/dts/
@@ -7,6 +7,8 @@ cp plutosdr_fw_patch/zynq-pluto-sdr-revplus.dts ../plutosdr-fw/linux/arch/arm/bo
 cp plutosdr_fw_patch/zynq-pluto-sdr.dtsi ../plutosdr-fw/linux/arch/arm/boot/dts/
 cp plutosdr_fw_patch/pluto.its ../plutosdr-fw/scripts
 cp plutosdr_fw_patch/pluto.mk ../plutosdr-fw/scripts
+
+cp plutosdr_fw_patch/uboot/zynq-common.h ../plutosdr-fw/u-boot-xlnx/include/configs
 
 cp plutosdr_fw_patch/linux/ad9361_regs.h ../plutosdr-fw/linux/drivers/iio/adc/
 cp plutosdr_fw_patch/linux/ad9361.c ../plutosdr-fw/linux/drivers/iio/adc/
